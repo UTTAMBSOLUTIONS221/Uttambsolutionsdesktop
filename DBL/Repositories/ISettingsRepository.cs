0cdf8ddc757dd9d9780c9783d1c078e2122f0f9e
Francis Kingori <158550717+Uttambsolution@users.noreply.github.com>
2024-06-04 15:24:45 +0300

    update

--- DBL/Repositories/ISettingsRepository.cs
+++ DBL/Repositories/ISettingsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace DBL.Repositories
 {
     public interface ISettingsRepository
     {
+        IEnumerable<SystemUoms> GetAllSystemUoms();
+        IEnumerable<SystemTaxCategory> SystemTaxCategory();
     }
 }
--- DBL/Repositories/SettingsRepository.cs
+++ DBL/Repositories/SettingsRepository.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using DBL.Models;
+using System.Data.SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace DBL.Repositories
 {
@@ -11,5 +14,39 @@ namespace DBL.Repositories
         public SettingsRepository(string connectionString) : base(connectionString)
         {
         }
+        #region UOMS
+        public IEnumerable<SystemUoms> GetAllSystemUoms()
+        {
+            using (var connection = new SQLiteConnection(_connString))
+            {
+                connection.Open();
+
+                // Execute the query to fetch Uoms
+                var uomQueryResult = connection.Query<SystemUoms>(
+                    @"SELECT Uomid,UomName,UomSymbol FROM UnitOfMeasure");
+
+                // Return the result
+                return uomQueryResult;
+            }
+        }
+        #endregion
+
+
+        #region Tax Categories
+        public IEnumerable<SystemTaxCategory> SystemTaxCategory()
+        {
+            using (var connection = new SQLiteConnection(_connString))
+            {
+                connection.Open();
+
+                // Execute the query to fetch tax categories
+                var taxCategoryQueryResult = connection.Query<SystemTaxCategory>(
+                    @"SELECT Uomid,UomName,UomSymbol FROM UnitOfMeasure");
+
+                // Return the result
+                return taxCategoryQueryResult;
+            }
+        }
+        #endregion
     }
 }
