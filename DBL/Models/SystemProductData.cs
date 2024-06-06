8641f0b2b9efd07d2659365747a74e96f0ad047e
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-06-06 07:44:05 +0300

    update

--- DBL/Models/SystemProductData.cs
+++ DBL/Models/SystemProductData.cs
@@ -12,15 +12,24 @@ namespace DBL.Models
         public string? ProductName { get; set; }
         public int UomId { get; set; }
         public string? UomName { get; set; }
-        public int CategoryId { get; set; }
-        public string? CategoryName { get; set; }
+        public int MainCategoryId { get; set; }
+        public string? MainCategoryName { get; set; }
+        public int FirstCategoryId { get; set; }
+        public string? FirstCategoryName { get; set; }
+        public int BrandId { get; set; }
+        public string? BrandName { get; set; }
         public int TaxCategoryId { get; set; }
         public string? TaxCategoryName { get; set; }
         public string? Barcode { get; set; }
-        public decimal Units { get; set; }
-        public decimal Price { get; set; }
-        public string? CreatedBy { get; set; }
-        public string? ModifiedBy { get; set; }
+        public double ProductUnits { get; set; }
+        public double WholeSalePrice { get; set; }
+        public double RetailSalePrice { get; set; }
+        public double ProfitMargin { get; set; }
+        public string? ProductSize { get; set; }
+        public string? ProductColor { get; set; }
+        public double ProductWeight { get; set; }
+        public int Createdby { get; set; }
+        public int Modifiedby { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
     }
